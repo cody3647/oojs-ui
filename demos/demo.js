ab7c9dda58549710820101bc30ffc24e6c912208
Cody Williams <williams.c@gmail.com>
2021-06-02 18:21:56 -0500

    First pass at Omen (dark with cyan primary) theme

--- demos/demo.js
+++ demos/demo.js
@@ -195,6 +195,7 @@ Demo.static.pages = {};
  * @property {Object.<string,string>}
  */
 Demo.static.themes = {
+	omen: 'Omen',
 	wikimediaui: 'WikimediaUI', // Do not change this line or you'll break `grunt add-theme`
 	apex: 'Apex'
 };
